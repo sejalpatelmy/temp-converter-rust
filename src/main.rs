52f98d44da2a42b26361f186015bde06708f7288
sayed belal <sayedbelal@sayeds-MacBook-Air.local>
2026-07-03 18:44:21 +0530

    code change

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ fn main() {
     println!("Hello, world!");
 
     println!("Welcome to temp convert");
-    println!("We convert Celicus to Farenhit");
+    println!("We convert Celsius to Fahrenheit");
+
     let mut degree = String::new();
-    io::stdin().read_line(&mut degree).expect("Failed to read the Degree");
-    let degree: f64 = degree.trim().parse().expect("please enter valid number");
 
-    println!("number entered is {}",degree);
-    println!("number is correct");
-    println!("converting it to degree celcius {} to farenhit ", degree);
-   let far: i32 = degree  * 1.8 + 32;
-    
-    let check = degree * 1.8 ; 
-    println!("farhente is {} ",check);
+    io::stdin()
+        .read_line(&mut degree)
+        .expect("Failed to read the degree");
 
-    println!("farhente is {} ",far);
+    let degree: f64 = degree
+        .trim()
+        .parse()
+        .expect("Please enter a valid number");
+
+    println!("Number entered is {}", degree);
+    println!("Number is correct");
+    println!(
+        "Converting {} Celsius to Fahrenheit...",
+        degree
+    );
+
+    let far = degree * 1.8 + 32.0;
+
+    println!("Fahrenheit is {}", far);
 }
